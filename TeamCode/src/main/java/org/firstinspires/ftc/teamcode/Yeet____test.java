ea6b240f10ad6fedff2a9effcf128eb658a083a1
RechargedRobotics <pellarobotics@gmail.com>
2020-02-04 07:28:15 -0600

    we can get block in Auto now 2=

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Yeet____test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Yeet____test.java
@@ -22,7 +22,7 @@ public class Yeet____test extends SuperDrive {
         initialization(false);
         waitForStart();
         while (opModeIsActive()) {
-            mecanumDrive();  //how to do suuf
+            mecanumDrive();  //how to do stuff
             CollectRight();
             CollectLeft();
             CollectBack();
